77d22cd2ed64a3aec42cb9706296c4c7da697ddc
donguk <ldu2175@gmail.com>
2021-03-23 20:34:33 +0900

    Initial Commit

---
+++ DUTodo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DUTodo
+//
+//  Created by 이동욱 on 2021/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DUTodo/DUTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DUTodoApp.swift
+//  DUTodo
+//
+//  Created by 이동욱 on 2021/03/23.
+//
+
+import SwiftUI
+
+@main
+struct DUTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
